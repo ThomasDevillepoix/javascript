0e2e35200af2855038766fcf464399e36d8540e6
ThomasDevillepoix <thomas.devillepoix@laposte.net>
2020-11-03 11:19:45 +0100

    Exo 5

---
+++ Exercice5/assets/script.js
@@ -0,0 +1,8 @@
+
+  document.getElementById('clickbutton').onclick =showAlert;
+  function showAlert(){
+      var num1 = document.getElementById('num1').value;
+      var num2 = document.getElementById('num2').value;
+      var resultat = ( num1%num2 )
+      alert('Reste' + resultat);
+  }
