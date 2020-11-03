6b050577400be35f945bc9245a6a50d65615a945
ThomasDevillepoix <thomas.devillepoix@laposte.net>
2020-11-03 11:10:05 +0100

    Exo 4

---
+++ Exercice4/assets/script.js
@@ -0,0 +1,8 @@
+document.getElementById('clickbutton').onclick =showAlert;
+ function showAlert(){
+     var nom = document.getElementById('nom').value ;
+     var prenom = document.getElementById('prenom').value;
+     var ville = document.getElementById('ville')
+     alert(' bonjour'+' ' + nom +' '+prenom );
+ }
+                                         
