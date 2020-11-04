06ff72e12caa33a86fa553a5ef912b24746f9e2a
ThomasDevillepoix <thomas.devillepoix@laposte.net>
2020-11-04 12:23:57 +0100

    Exo1 Partie2

---
+++ Partie2/Exercice1/assets/script.js
@@ -0,0 +1,8 @@
+function setNewImage()
+{
+    document.getElementById("img1").src ="assets/images/noyade.jpg";
+}
+function setOldImage()
+{
+    document.getElementById('img1').src ="assets/images/ile.jpg";
+}
